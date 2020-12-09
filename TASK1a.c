afa9a42a02f7ad674084e5f5e315892825087ba0
scyyf1 <scyyf1@CSLinux.nottingham.edu.cn>
2020-12-09 23:17:17 +0800

    first commit

---
+++ TASK1a.c
@@ -0,0 +1,12 @@
+#include "coursework.h"
+#include "linkedlist.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    struct process *processesHand;
+    /* code */
+    return 0;
+}
+
+
